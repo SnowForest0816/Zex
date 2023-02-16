00d0f820495409427f3f2314cf7dd4e3a0aee385
SnowForest0816 <snowforest0816@protonmail.com>
2023-02-16 05:44:23 -0800

    update

---
+++ src/components/trade-page/trade-page-header/TradePageHeader.jsx
@@ -0,0 +1,125 @@
+import { Divider, Select } from 'antd';
+<<<<<<< HEAD
+import { React } from 'react';
+=======
+import React, { useState, useEffect } from 'react';
+import { BASE_URL } from '../../../constants/networks'
+>>>>>>> a1f256f9991ad5cb98670d606394085d33bc8a84
+
+function TradePageHeader(props) {
+    const handleChange = (value) => {
+        console.log(`selected ${value}`);
+    };
+    const { data, percent } = useCoinMarket();
+    return (
+        <div>
+            <div className='flex items-center py-3 mflex-wrap gap-5'>
+                <div className='flex mw-50 mmb-4'>
+                    <div className='self-center'>
+                        <img className='mr-3 h-8' src='https://mux-world.github.io/assets/img/tokens/MATIC.svg' alt='token'/>
+                    </div>
+                    <div className='mr-2'>
+                        <div className='text-white font-semibold'>MATIC</div>
+                        <div className='text-xs whitespace-nowrap tp'>
+                            Up to 100X
+                        </div>
+                    </div>
+                    <div className='self-center text-white'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" >
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <polyline points="6 9 12 15 18 9"></polyline>
+                        </svg>
+                    </div>
+                </div>
+                <Divider type="vertical" className='m-view-dnone' />
+                <div className='mw-50 mtext-right mmb-4'>
+<<<<<<< HEAD
+                    <span className='font-bold text-xl text-white mr-3'>{ props.currentData.close }</span>
+                    <span className='text-base text-red-600'>(- 0.33%)</span>
+=======
+                    <span className='font-bold text-xl text-white mr-3'>{data}</span>
+                    <span className={percent > 0 ? 'text-base text-green-600' : 'text-base text-red-600'}>({percent} %)</span>
+>>>>>>> a1f256f9991ad5cb98670d606394085d33bc8a84
+                </div>
+                <Divider type="vertical" className='m-view-dnone' />
+                <div className='text-xs mw-33 mmb-4 mtext-center mp-0'>
+                    <div className='tp whitespace-pre-wrap'>Est. 1H Funding (L/S)</div>
+                    <div className='text-white'>0.030% / 0.011%</div>
+                </div>
+                <Divider type="vertical" />
+                <div className='text-xs mw-33 mmb-4 mtext-center mp-0'>
+                    <div className='tp whitespace-pre-wrap'>Rollover</div>
+                    <div className='text-white'>0.0146%</div>
+                </div>
+                <Divider type="vertical" />
+                <div className='text-xs mw-33 mmb-4 mp-0'>
+                    <div className='tp whitespace-pre-wrap'>Long / Short (Aggregator)</div>
+                    <div className='text-white'> $0 / $0</div>
+                </div>
+                {!process.env.REACT_APP_MAINNET && <><Divider type="vertical" />
+                <div className='text-xs mw-33 mmb-4 mtext-right mp-0'>
+                    <div className='tp whitespace-pre-wrap text-right absolute top-[15px] right-2'>Price Source</div>
+                    <div className='text-right absolute right-0'>
+                        <Select
+                            className='cus-select cus-select-sm text-white font-medium'
+                            style={{
+                                minWidth: "142px"
+                            }}
+                            defaultValue="MATIC"
+                            onChange={handleChange}
+                            options={[
+                                {
+                                    value: 'auto',
+                                    label: 'Auto Match',
+                                },
+                                {
+                                    value: 'mux',
+                                    label: 'MUX',
+                                },
+                                {
+                                    value: 'gmx',
+                                    label: 'GMX',
+                                },
+                                {
+                                    value: 'gains',
+                                    label: 'Gains',
+                                },
+                            ]}
+                        />
+                    </div>
+                </div></>}
+            </div>
+        </div>
+    );
+}
+
+export default TradePageHeader;
+
+export function useCoinMarket() {
+    const [state, setState] = useState({ data: [], isLoading: true });
+    const updateState = (data, percent) => {
+        setState(state => ({
+            data: data ? data.MATIC.quote.USD.price.toFixed(4) : state.data,
+            percent: percent ?  data.MATIC.quote.USD.percent_change_24h.toFixed(2) : data.MATIC.quote.USD.percent_change_24h.toFixed(2),
+            isLoading: false,
+        }));
+    };
+    async function init() {
+        try {
+            const res = await fetch(`${BASE_URL}/api`);
+            const data = await res.json();
+            console.log(data, 'data')
+            updateState(data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    useEffect(() => {
+        init();
+        const id = setInterval(() => {
+            init();
+        }, 1 * 60 * 10);
+        return () => clearInterval(id);
+    }, []);
+    return state;
+}
---
+++ src/components/trade-page/trade-page-right-bar/TradePageRightBar.jsx
@@ -0,0 +1,336 @@
+import { Button, Card, Col, Divider, Input, InputNumber, Radio, Row, Select, Slider, Tooltip } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { useAccount, useContractRead, useContractWrite, usePrepareContractWrite } from 'wagmi';
+// import { usePrepareSendTransaction, useSendTransaction } from 'wagmi';
+// import { getProviders } from '../../../utils/common';
+import zexStorageABI from '../../../config/abis/zexStorage.json';
+import { ethers } from 'ethers';
+
+function TradePageRightBar({setIsModalOpen, currentData}) {
+    const { isConnected } = useAccount();
+    
+    const fee = 0.08;
+    const price = currentData.close;
+
+    const [ limitPrice, setLPrice ] = useState(price);
+    const [ stopPrice, setSPrice ] = useState(price);
+    const [ usedAmount, setUsedAmount ] = useState();
+    const [ resultAmount, setResultAmount ] = useState();
+    
+
+    const changeUsedAmount = (value) => {
+        setUsedAmount(value);
+        let amount = ((value * (1 - fee)) / price) * inputValue;
+        if (amount === 0) {
+            setResultAmount();
+        } else {
+            amount = amount.toFixed(4);
+            setResultAmount(amount);
+        }
+    }
+
+    const changeResultAmount = (value) => {
+        setResultAmount(value);
+        let amount = (value * price) / (1 - fee);
+        if (amount === 0) {
+            setUsedAmount();
+        } else {
+            amount = amount.toFixed(4);
+            setUsedAmount(amount);
+        }
+    }
+
+    const [ status, setStatus ] = useState('market');
+
+    const handleChange = (value) => {
+        setStatus(value);
+        setLPrice(price);
+        setSPrice(price);
+    };
+
+    const [value, setValue] = useState(1);
+    const onChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    // Slider range
+    const marks = {
+        1: '1x',
+        25: '25x',
+        50: '50x',
+        75: '75x',
+        100: '100x',
+    };
+
+    const [inputValue, setInputValue] = useState(1);
+    const onChange1 = (newValue) => {
+        setInputValue(newValue);
+        let amount = ((value * (1 - fee)) / price) * newValue;
+        if (amount === 0) {
+            setResultAmount();
+        } else {
+            amount = amount.toFixed(4);
+            setResultAmount(amount);
+        }
+    };
+
+    useEffect(()=>{
+        // console.log(contract, '------c----')
+    }, []);
+
+    const { config } = usePrepareContractWrite({
+        address: '0xA187457BAc9a236989c9052ffC7619Cb5A6eE1Ea',
+        abi: zexStorageABI,
+        functionName: 'placePositionOrder2',
+        args: ["0x494239ffe9057d05ad5f8c4a23a58f306bb13e9b010101000000000000000000", 100000000000000, 990000000000000, 0, 0, 128, 8640000, "0x0000000000000000000000000000000000000000000000000000000000000000"],
+        overrides:{
+            gasLimit:1000000,
+            value: ethers.utils.parseEther('0.01'),
+        }
+    })
+    const { data, isLoading, isSuccess, write, error } = useContractWrite(config)
+
+    const { data: maintainer, isError, isLoading: maintainerLoading } = useContractRead({
+        address: '0xA187457BAc9a236989c9052ffC7619Cb5A6eE1Ea',
+        abi: zexStorageABI,
+        functionName: 'owner',
+    })
+<<<<<<< HEAD
+
+=======
+    const buy = () => {
+
+    }
+
+    const sell = () => {
+
+    }
+>>>>>>> a1f256f9991ad5cb98670d606394085d33bc8a84
+    return (
+        <div className='trade-page-right-bar'>
+            <div className='trade-page-right-bar-header p-3'>
+                <div className='flex items-center justify-between'>
+                    <Button className='zex-btn-sec' size='large'disabled={!write} onClick={() => write?.()} >Isolated</Button> {/**/}
+
+                    <Radio.Group className='cus-radio-group' size="large" onChange={onChange} value={value} defaultValue="1">
+                        <Radio.Button value={1} onClick={buy}>Buy / Long</Radio.Button>
+                        <Radio.Button className='orange' value={2} onClick={sell}>Sell / Short</Radio.Button>
+                    </Radio.Group>
+                    <Divider type="vertical" />
+                    <Button size='large' className='tp border-0 zex-btn-sec'>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-adjustments-horizontal" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <circle cx="14" cy="6" r="2"></circle>
+                            <line x1="4" y1="6" x2="12" y2="6"></line>
+                            <line x1="16" y1="6" x2="20" y2="6"></line>
+                            <circle cx="8" cy="12" r="2"></circle>
+                            <line x1="4" y1="12" x2="6" y2="12"></line>
+                            <line x1="10" y1="12" x2="20" y2="12"></line>
+                            <circle cx="17" cy="18" r="2"></circle>
+                            <line x1="4" y1="18" x2="15" y2="18"></line>
+                            <line x1="19" y1="18" x2="20" y2="18"></line>
+                        </svg>
+                    </Button>
+                </div>
+            </div>
+            <div className='trade-page-right-bar-body p-3'>
+                <Row gutter={10}>
+                    <Col xs={12}>
+                        <Card className='zex-cus-card'>
+                            <Tooltip title="This position will be opened on ZEX.">
+                                <div className='market-icon-content'>
+                                    <img className='market-icon-postion-img' src='/img/zexicon.png' alt="mux"/>
+                                </div>
+                            </Tooltip>
+                            <div className='text-xs'>
+                                Market Price
+                            </div>
+<<<<<<< HEAD
+                            {
+                                status === 'market' 
+                                    ? <div className='text-lg font-bold'>$ { price }</div>
+                                    : (
+                                        status === 'limit'
+                                            ?   
+                                                <div className='flex justify-center items-center gap-x-2 font-bold'>
+                                                    <p className='text-white text-lg'>$</p>
+                                                    <Input className='cus-input' value={limitPrice} placeholder="0.0" onChange = {(e) => setLPrice(e.target.value)} />
+                                                </div>
+                                            : 
+                                                <div className='flex justify-center items-center gap-x-2 font-bold'>
+                                                    <p className='text-white text-lg'>≥$</p>
+                                                    <Input className='cus-input' value={stopPrice} placeholder="0.0" onChange = {(e) => setSPrice(e.target.value)} />
+                                                </div>
+                                    )
+                            }
+                            
+=======
+                            <div className='text-lg text-white font-bold'>
+                                $ 1.0745
+                            </div>
+>>>>>>> a1f256f9991ad5cb98670d606394085d33bc8a84
+                        </Card>
+                    </Col>
+                    <Col xs={12}>
+                        <Card className='zex-cus-card'>
+                            <div className='text-xs text-right'>
+                                Market Price
+                            </div>
+                            <div className='text-lg font-bold text-right'>
+                                <Select
+                                    className='cus-select'
+                                    style={{
+                                        width: "142px"
+                                    }}
+                                    defaultValue="MATIC"
+                                    onChange={handleChange}
+                                    options={[
+                                        {
+                                            value: 'market',
+                                            label: 'Market',
+                                        },
+                                        {
+                                            value: 'limit',
+                                            label: 'Limit',
+                                        },
+                                        {
+                                            value: 'stop-market',
+                                            label: 'Stop Market',
+                                        },
+                                    ]}
+                                />
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col xs={24}>
+                        <Card className='zex-cus-card'>
+                            <Row gutter={10} align="bottom">
+                                <Col xs={12}>
+                                    {
+                                        usedAmount > 0 
+                                            ?   <div className='text-xs '>
+                                                    Use: $ { usedAmount }
+                                                </div>
+                                            :   <div className='text-xs'>
+                                                    Use: $0
+                                                </div>
+                                    }
+                                    <div>
+                                        <Input className='cus-input' placeholder="0.0" value={ usedAmount } onChange={(e) => changeUsedAmount(e.target.value)} />
+                                    </div>
+                                </Col>
+                                <Col xs={12} >
+                                    <div className='text-right font-bold text-base whitespace-nowrap text-white flex place-content-end'>
+                                        Select Collateral
+                                        <svg xmlns="http://www.w3.org/2000/svg" className='ml-1' width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" >
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <polyline points="6 9 12 15 18 9"></polyline>
+                                        </svg>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Card>
+                    </Col>
+                    <Col xs={24}>
+                        <div className='to-arrow tp text-center'>
+                            <svg xmlns="http://www.w3.org/2000/svg" className='m-auto' width="20" height="20" viewBox="0 0 24 24" strokwidth="2" stroke="currentColor" fill="none" >
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <line x1="12" y1="5" x2="12" y2="19"></line>
+                                <line x1="18" y1="13" x2="12" y2="19"></line>
+                                <line x1="6" y1="13" x2="12" y2="19"></line>
+                            </svg>
+                        </div>
+                        <Card className='zex-cus-card'>
+                            <Row gutter={10} align="bottom">
+                                <Col xs={12}>
+                                    <div className='text-xs '>
+                                        {
+                                            value === 1 
+                                                ?   (
+                                                        (usedAmount * (1 - fee) * inputValue) > 0
+                                                            ? <p>Long: $ { (usedAmount * (1 - fee) * inputValue).toFixed(4) }</p>
+                                                            : <p>Long: $0</p>
+                                                )
+                                                :   (
+                                                        (usedAmount * (1 - fee) * inputValue) > 0
+                                                            ? <p>Short: $ { (usedAmount * (1 - fee) * inputValue).toFixed(4) }</p>
+                                                            : <p>Short: $0</p>
+                                                )
+                                        }
+                                    </div>
+                                    <div>
+                                        <Input className='cus-input' placeholder="0.0" value={ resultAmount } onChange={(e) => changeResultAmount(e.target.value)} />
+                                    </div>
+                                </Col>
+                                <Col xs={12} >
+                                    <div className='text-right font-bold text-base whitespace-nowrap text-white flex place-content-end'>
+                                        Select Market
+                                        <svg xmlns="http://www.w3.org/2000/svg" className='ml-1' width="20" height="20" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" >
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <polyline points="6 9 12 15 18 9"></polyline>
+                                        </svg>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Card>
+                        <Card className='leverage-card'>
+                            <div className='flex justify-between tp text-xs mb-4'>
+                                <div>Leverage: {inputValue}x</div>
+                                <div>
+                                    Shortcut
+                                </div>
+                            </div>
+                            <Row gutter={10}>
+                                <Col xs={6}>
+                                    <InputNumber
+                                        className='slider-input'
+                                        min={1}
+                                        max={100}
+                                        placeholder="1"
+                                        value={inputValue}
+                                        onChange={onChange1}
+                                    />
+                                </Col>
+                                <Col xs={18}>
+                                    <Slider marks={marks} defaultValue={1} onChange={onChange1}
+                                        value={typeof inputValue === 'number' ? inputValue : 0} />
+                                </Col>
+                            </Row>
+                        </Card>
+                    </Col>
+                </Row>
+                <div className='my-5'>
+                    {!isConnected && <Button className='w-full' size='large' type='primary' onClick={()=>setIsModalOpen(true)}>Connect Wallet</Button>}
+                </div>
+                <div>
+                    <div className='flex justify-between tp text-sm mb-2'>
+                        <div>Available Liquidity</div>
+                        <div className='text-white'>21.72846 MATIC</div>
+                    </div>
+                    <div className='flex justify-between tp text-sm mb-2'>
+                        <div>Profits In</div>
+                        <div className='text-white'>MATIC</div>
+                    </div>
+                    <div className='flex justify-between tp text-sm mb-2'>
+                        <div>Liq. Price</div>
+                        <div className='text-white'>--</div>
+                    </div>
+                    <div className='flex justify-between tp text-sm mb-2'>
+                        <div>Collateral</div>
+                        <div className='text-white'>--</div>
+                    </div>
+                    <div className='flex justify-between tp text-sm mb-2'>
+                        <div>Fees</div>
+                        <div className='text-white'>--</div>
+                    </div>
+                    <div className='flex justify-between tp text-sm mb-2'>
+                        <div>Spread</div>
+                        <div className='text-white'>0%</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TradePageRightBar;
